a4eb7f0206e9ade69ac13cddd6b0ad82c4c28314
xxhfg <xxhfg@163.com>
2011-10-28 13:18:13 +0800

    change database sql with default value

--- stock.sql
+++ stock.sql
@@ -1,12 +1,27 @@
-CREATE TABLE "gphq_bkdm" (
-    "BKDM" varchar(10) NOT NULL PRIMARY KEY,
-    "BKMC" varchar(10) NOT NULL
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE "gphq_gpdm" (
+    "SCDM" varchar(1) NOT NULL,
+    "GPDM" varchar(10) NOT NULL,
+    "SCJC" varchar(2) NOT NULL,
+    "GPMC" varchar(10) NOT NULL,
+    "CODE" varchar(10) NOT NULL PRIMARY KEY
 );
-CREATE TABLE "gphq_bkgp" (
+CREATE TABLE "gphq_rxhq" (
     "id" integer NOT NULL PRIMARY KEY,
-    "BKDM" varchar(10) NOT NULL,
     "CODE" varchar(10) NOT NULL,
-    UNIQUE ("BKDM", "CODE")
+    "JYRQ" date NOT NULL,
+    "OPEN" integer NOT NULL,
+    "HIGH" integer NOT NULL,
+    "LOW" integer NOT NULL,
+    "CLOSE" integer NOT NULL,
+    "PCLOSE" integer NOT NULL,
+    "PHIGH" integer NOT NULL,
+    "PLOW" integer NOT NULL,
+    "VOL" integer NOT NULL,
+    "AMT" integer NOT NULL,
+    "CHG" real NOT NULL,
+    UNIQUE ("CODE", "JYRQ")
 );
 CREATE TABLE "gphq_fzhq" (
     "id" integer NOT NULL PRIMARY KEY,
@@ -20,28 +35,10 @@ CREATE TABLE "gphq_fzhq" (
     "VOL" integer NOT NULL,
     "AMT" integer NOT NULL
 );
-CREATE TABLE "gphq_gbbq" (
-    "id" integer NOT NULL PRIMARY KEY,
-    "CODE" varchar(10) NOT NULL,
-    "BGRQ" date NOT NULL,
-    "TOTAL" integer NOT NULL,
-    "FLOW" integer NOT NULL,
-    "REAL" integer NOT NULL,
-    UNIQUE ("CODE", "BGRQ")
+CREATE TABLE "gphq_zdrq" (
+    "JYRQ" date NOT NULL PRIMARY KEY
 );
-CREATE TABLE "gphq_gnbk" (
-    "id" integer NOT NULL PRIMARY KEY,
-    "GNLX" varchar(10) NOT NULL,
-    "BKDM" varchar(10) NOT NULL,
-    UNIQUE ("GNLX", "BKDM")
-);
-CREATE TABLE "gphq_gpdm" (
-    "SCDM" varchar(1) NOT NULL,
-    "GPDM" varchar(10) NOT NULL,
-    "SCJC" varchar(2) NOT NULL,
-    "GPMC" varchar(10) NOT NULL,
-    "CODE" varchar(10) NOT NULL PRIMARY KEY
-);
+INSERT INTO "gphq_zdrq" VALUES('1900-01-01');
 CREATE TABLE "gphq_gprq" (
     "id" integer NOT NULL PRIMARY KEY,
     "CODE" varchar(10) NOT NULL,
@@ -73,41 +70,40 @@ CREATE TABLE "gphq_kzhq" (
     "SIG" integer NOT NULL,
     UNIQUE ("CODE", "JYRQ")
 );
-CREATE TABLE "gphq_rxhq" (
+CREATE TABLE "gphq_gbbq" (
     "id" integer NOT NULL PRIMARY KEY,
     "CODE" varchar(10) NOT NULL,
-    "JYRQ" date NOT NULL,
-    "OPEN" integer NOT NULL,
-    "HIGH" integer NOT NULL,
-    "LOW" integer NOT NULL,
-    "CLOSE" integer NOT NULL,
-    "PCLOSE" integer NOT NULL,
-    "PHIGH" integer NOT NULL,
-    "PLOW" integer NOT NULL,
-    "VOL" integer NOT NULL,
-    "AMT" integer NOT NULL,
-    "CHG" real NOT NULL,
-    UNIQUE ("CODE", "JYRQ")
+    "BGRQ" date NOT NULL,
+    "TOTAL" integer NOT NULL,
+    "FLOW" integer NOT NULL,
+    "REAL" integer NOT NULL,
+    UNIQUE ("CODE", "BGRQ")
 );
-CREATE TABLE "gphq_zdrq" (
-    "JYRQ" date NOT NULL PRIMARY KEY
+CREATE TABLE "gphq_bkdm" (
+    "BKDM" varchar(10) NOT NULL PRIMARY KEY,
+    "BKMC" varchar(10) NOT NULL
 );
+CREATE TABLE "gphq_bkgp" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "BKDM" varchar(10) NOT NULL,
+    "CODE" varchar(10) NOT NULL,
+    UNIQUE ("BKDM", "CODE")
+);
+CREATE TABLE "gphq_gnbk" (
+    "id" integer NOT NULL PRIMARY KEY,
+    "GNLX" varchar(10) NOT NULL,
+    "BKDM" varchar(10) NOT NULL,
+    UNIQUE ("GNLX", "BKDM")
+);
+CREATE INDEX "gphq_rxhq_ec2f0154" ON "gphq_rxhq" ("CODE");
+CREATE INDEX "gphq_rxhq_28cc79d" ON "gphq_rxhq" ("JYRQ");
+CREATE INDEX "gphq_fzhq_ec2f0154" ON "gphq_fzhq" ("CODE");
 CREATE INDEX "gphq_fzhq_28cc79d" ON "gphq_fzhq" ("JYRQ");
 CREATE INDEX "gphq_fzhq_9268833c" ON "gphq_fzhq" ("TJSD");
-CREATE INDEX "gphq_fzhq_ec2f0154" ON "gphq_fzhq" ("CODE");
-CREATE INDEX "gphq_gbbq_a31cf595" ON "gphq_gbbq" ("BGRQ");
-CREATE INDEX "gphq_gbbq_ec2f0154" ON "gphq_gbbq" ("CODE");
-CREATE INDEX "gphq_kzhq_28cc79d" ON "gphq_kzhq" ("JYRQ");
 CREATE INDEX "gphq_kzhq_ec2f0154" ON "gphq_kzhq" ("CODE");
-CREATE INDEX "gphq_rxhq_28cc79d" ON "gphq_rxhq" ("JYRQ");
-CREATE INDEX "gphq_rxhq_ec2f0154" ON "gphq_rxhq" ("CODE");
-CREATE TRIGGER "insert_lastday"
-   BEFORE   INSERT 
-   ON gphq_rxhq
-   when (select count(*) from gphq_gprq where code=new.code)=0
-BEGIN
-    insert into gphq_gprq(code, jyrq) values(new.code, new.jyrq);
-END;
+CREATE INDEX "gphq_kzhq_28cc79d" ON "gphq_kzhq" ("JYRQ");
+CREATE INDEX "gphq_gbbq_ec2f0154" ON "gphq_gbbq" ("CODE");
+CREATE INDEX "gphq_gbbq_a31cf595" ON "gphq_gbbq" ("BGRQ");
 CREATE TRIGGER "update_lastday"
    AFTER   INSERT 
    ON gphq_rxhq
@@ -115,9 +111,17 @@ CREATE TRIGGER "update_lastday"
 BEGIN
     update gphq_gprq set jyrq=new.jyrq where code=new.code and jyrq<new.jyrq;
 END;
+CREATE TRIGGER "insert_lastday"
+   BEFORE   INSERT 
+   ON gphq_rxhq
+   when (select count(*) from gphq_gprq where code=new.code)=0
+BEGIN
+    insert into gphq_gprq(code, jyrq) values(new.code, new.jyrq);
+END;
 CREATE TRIGGER "update_maxday"
    AFTER   UPDATE OF JYRQ
    ON gphq_gprq
 BEGIN
     update gphq_zdrq set jyrq=new.jyrq where jyrq<new.jyrq;
 END;
+COMMIT;
